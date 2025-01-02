386749a13ba33594222c0cb3e2453b7481b032a8
Flinguee75 <redfoo923@gmail.com>
2025-01-02 13:19:08 -0500

    La base de donnée a été creé, place au travail sur le dataset

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from .config import Config
-from .models import init_app
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    init_app(app)
     return app
